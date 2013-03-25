ca8b8a135cb548c558dbd55158889febef82461f
Zachary Latta <zchlatta@gmail.com>
2013-03-24 20:39:30 -0700

    Added homework-4 program.

---
+++ homework-4/draw_text_colors.asm
@@ -0,0 +1,18 @@
+title Draw Text Colors						(draw_text_colors.asm)
+
+INCLUDE Irvine32.inc
+.data
+string byte "This string is colored green with a black background.",0dh,0ah,0
+
+.code
+main proc
+	mov ax,green + (black * 16)
+    call SetTextColor
+
+    mov edx,offset string
+    call WriteString
+
+	exit
+main endp
+
+end main
